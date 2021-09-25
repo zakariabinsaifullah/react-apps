faced77383630f2bc6339551c625622b141eccea
zakariabinsaifullah <zbinsaifullah@gmail.com>
2021-09-25 22:50:52 +0600

    files are added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import { useState } from "react";
 
 function App() {
+   const [text, setText] = useState([])
+   const [amount, setAmount] = useState(0);
+
+   //zero value alert
+    let message = "";
+    if (amount <=0 || amount > 6 ){
+        message = "Paragraph Number Min Value is 1 and Max Value is 6";
+    }
+   // generate button
+   const onGenerate = () => {
+       setText(data.slice(0, parseInt(amount)));
+   }
+
+  // oninput button
+    const onInputSet = (e) => {
+       setAmount(e.target.value);
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-title"> Random Text Generator </div>
+        <div className="app-generator">
+            <strong>Paragraphs: </strong>
+            <input type="number" name="paragraph" min="1" max="6" onChange={ onInputSet } value={ amount }/>
+            <button onClick={ onGenerate }>Generate</button>
+        </div>
+            <div className="app-content">
+                {
+                    (text && amount > 0) &&
+                    text.map((text, index) => {
+                        return(
+                            <p className="single-para" key={index}>
+                                { text }
+                            </p>
+                        )
+                    })
+                }
+                {
+                    ( amount <= 0 || amount > 6 ) &&
+                    <div className="red">
+                        { message }
+                    </div>
+                }
+            </div>
     </div>
   );
 }
